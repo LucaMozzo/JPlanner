6d6a5b56ad2bd5577c2c0ade2f84a89d49453f2a
lucamozzo <lucamozzo21173@gmail.com>
2016-05-04 16:43:23 +0100

    Added javadoc and method to get all the instance variables

--- src/plan/problem/Problem.java
+++ src/plan/problem/Problem.java
@@ -1,6 +1,9 @@
 package plan.problem;
 
 import plan.domain.Domain;
+import plan.domain.Variable;
+
+import java.util.LinkedList;
 
 /**
  * Created by LUCA on 01/05/2016.
@@ -10,9 +13,22 @@ import plan.domain.Domain;
 public class Problem {
 
     private Domain domain;
+    private LinkedList<Variable> variables;
 
+    /**
+     * Default constructor
+     * @param domain the domain
+     */
     public Problem(Domain domain){
 
         this.domain = domain;
+    }
+
+    /**
+     * Return all the variables of this instance
+     * @return the variables
+     */
+    public LinkedList<Variable> getInstanceVariables(){
+        return variables;
     }
 }
