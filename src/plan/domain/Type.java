fb4d0d5ccb434db0431fc17601b76ecaba8b1cf5
lucamozzo <lucamozzo21173@gmail.com>
2016-05-05 16:53:27 +0100

    Created Type class

--- src/plan/domain/Type.java
+++ src/plan/domain/Type.java
@@ -2,6 +2,39 @@ package plan.domain;
 
 /**
  * Created by LUCA on 01/05/2016.
+ *
+ * A data type in the plan (unique in each domain, identified by the name)
  */
 public class Type {
+
+    private String name;
+
+    /**
+     * The constructor only takes the name as argument
+     * @param name the name
+     */
+    public Type(String name){
+        this.name = name;
+    }
+
+    /**
+     * Getter for the name
+     * @return the name
+     */
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object otherType){
+        if(otherType instanceof Type)
+            return name.equals(((Type)otherType).getName());
+        else
+            return false;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
 }
