d3ebed45456619e136d7517c875497fc72f2d209
lucamozzo <lucamozzo21173@gmail.com>
2016-05-18 10:15:27 +0100

    Added support for comparison operators in preconditions

--- src/planner/domain/Precondition.java
+++ src/planner/domain/Precondition.java
@@ -1,5 +1,7 @@
 package planner.domain;
 
+import utils.ComparisonOperator;
+
 /**
  * Created by LUCA on 01/05/2016.
  *
@@ -7,13 +9,27 @@ package planner.domain;
  */
 public class Precondition<E> extends Fact<E> {
 
+    private ComparisonOperator operator;
+
     /**
      * The constructor takes the variable and its expected value
      * @param var the variable
      * @param expValue the expected value
      */
     public Precondition(Variable var, E expValue){
+        this(var, expValue, ComparisonOperator.EQUAL); //by default we set the equals operator
+    }
+
+    /**
+     * The constructor takes the variable, its expected value and the operator
+     * @param var the variable
+     * @param expValue the expected value
+     * @param operator the operator
+     */
+    public Precondition(Variable var, E expValue, ComparisonOperator operator){
         super(var, expValue);
+
+        this.operator = operator;
     }
 
     /**
---
+++ src/utils/ComparisonOperator.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by LUCA on 18/05/2016.
+ *
+ * ComparisonOperator operators
+ */
+public enum ComparisonOperator {
+    EQUAL, GREATER, LESS, GREATER_OR_EQUAL, LESS_OR_EQUAL
+}
