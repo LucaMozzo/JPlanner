5a4a5a2458aedf561ef47f93624b580886213791
lucamozzo <lucamozzo21173@gmail.com>
2016-05-20 22:22:17 +0100

    Added iterator for the tree

--- src/planner/searchspace/datastructures/Tree.java
+++ src/planner/searchspace/datastructures/Tree.java
@@ -1,13 +1,16 @@
 package planner.searchspace.datastructures;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Created by LUCA on 17/05/2016.
  *
- * Tree supporting all kinds of nodes
+ * Iterable tree of nodes
  */
-public class Tree implements NodeTree {
+public class Tree implements NodeTree, Iterable {
 
     protected Node root;
     protected long size = 0;
@@ -60,5 +63,57 @@ public class Tree implements NodeTree {
     @Override
     public boolean isLeaf(Node node) {
         return node.isLeaf();
+    }
+
+    @Override
+    public Iterator iterator() {
+
+        /*
+        The currentNode should never become null during the iteration, except when it's the first iteration, the one
+        that precedes the root
+         */
+
+        Iterator iterator = new Iterator() {
+
+            private Node currentNode = null;
+            private Stack<Node> stack = new Stack<>();
+
+            @Override
+            public boolean hasNext() {
+                if(currentNode == null)
+                    return true; //first iteration
+                else
+                    return preorder(currentNode) != null;
+            }
+
+            @Override
+            public Node next() {
+                if(currentNode == null) {
+                    stack.push(root);
+                    return currentNode = root; //the first call
+                }
+                else {
+                    //prevent the currentNode to become null when the tree has finished (can be confused with th1st iteration)
+                    currentNode = (preorder(currentNode) == null ? currentNode : preorder(currentNode));
+                    return currentNode;
+                }
+            }
+
+            private Node preorder(Node prev){
+                if(!stack.isEmpty())
+                    if(prev.isLeaf()){ //get siblings/backtrack
+                        return stack.pop();
+                    }
+                    else{
+                        //push all the children of the current node and pop the first
+                        for(Node child : (LinkedList<Node>)prev.getChildren())
+                            stack.push(child);
+                        return stack.pop();
+                    }
+                else
+                    return null;
+            }
+        };
+        return iterator;
     }
 }
