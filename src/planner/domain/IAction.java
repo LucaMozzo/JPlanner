01839ad643027973891bd64928ce549b7ac2a3ee
Luca <lucamozzo21173@gmail.com>
2016-07-13 10:50:25 +0200

    interface for all kind of actions

---
+++ src/planner/domain/IAction.java
@@ -0,0 +1,24 @@
+package planner.domain;
+
+import planner.problem.State;
+
+/**
+ * Created by LUCA on 13/07/2016.
+ *
+ * An interface that contains the methods that every action should have
+ */
+public interface IAction {
+
+    /**
+     * Determines whether the action is currently applicable, by checking preconditions
+     * @param state the state on which check if applicable
+     * @return true if applicable
+     */
+    boolean isApplicable(State state);
+
+    /**
+     * Applies all the effects after it is performed
+     * @param state the state on which apply the effects
+     */
+    void applyEffects(State state);
+}
