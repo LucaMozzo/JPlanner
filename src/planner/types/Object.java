e2087cde2a5ea78d7f00cdb2d20ecd1df3df1ef0
Luca <lucamozzo21173@gmail.com>
2016-07-13 14:13:35 +0200

    Added check on variable names and added matches method

--- src/planner/types/Object.java
+++ src/planner/types/Object.java
@@ -3,6 +3,7 @@ package planner.types;
 import planner.domain.Variable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 /**
@@ -18,7 +19,10 @@ public abstract class Object {
      * Add a property to the list
      * @param property the property to be added
      */
-    public void addProperty(Variable property){
+    public void addProperty(Variable property) throws Exception{
+        for(Variable v : properties)
+            if(property.getName().equals(v.getName()))
+                throw new Exception("No 2 variables can have the same name");
         properties.add(property);
     }
 
@@ -35,7 +39,7 @@ public abstract class Object {
      * @param name the name
      * @return the property
      */
-    public Variable getProperty(String name) {
+    public Variable getPropertyByName(String name) {
         for(Variable v : properties)
             if(v.getName().equals(name))
                 return v;
@@ -47,12 +51,27 @@ public abstract class Object {
      * @param name the name of the property
      * @param newValue the new value
      */
-    public void setProperty(String name, DefaultDataType newValue) throws Exception{
+    public void setProperty(String name, DefaultDataType newValue) throws Exception {
         for(Variable v : properties)
             if(v.getName().equals(name)) {
                 v.setValue(newValue);
                 return;
             }
         throw new Exception("No variable called \"" + name + "\" in current context");
+    }
+
+    /**
+     * Two objects match if they have the same properties with the same values (different from equals)
+     * @param other the other Object
+     * @return whether they match
+     */
+    public boolean matches(Object other){
+        Iterator it = properties.iterator();
+        Iterator otherIt = other.properties.iterator();
+
+        while(it.hasNext())
+            if(!it.next().equals(otherIt.next()))
+                return false;
+        return true;
     }
 }
